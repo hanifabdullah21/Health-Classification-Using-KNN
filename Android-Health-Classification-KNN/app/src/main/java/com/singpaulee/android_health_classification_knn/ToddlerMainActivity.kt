bfdbbba49ebd61f579ea2308c8792163f54faf57
hanif <hanifabdullah7@gmail.com>
2019-09-20 15:59:20 +0700

    [Android] Add Menu Utama Balita

---
+++ Android-Health-Classification-KNN/app/src/main/java/com/singpaulee/android_health_classification_knn/ToddlerMainActivity.kt
@@ -0,0 +1,12 @@
+package com.singpaulee.android_health_classification_knn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ToddlerMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_toddler_main)
+    }
+}
