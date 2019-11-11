2cd7fdde18504d43697381246d5dee0fdb6bc8f8
hanif <hanifabdullah7@gmail.com>
2019-11-11 22:52:16 +0700

    Change percentage toddler to bar chart

--- Android-Health-Classification-KNN/app/src/main/java/com/singpaulee/android_health_classification_knn/mvp/toddlerrpercentage/PercentageToddlerActivity.kt
+++ Android-Health-Classification-KNN/app/src/main/java/com/singpaulee/android_health_classification_knn/mvp/toddlerrpercentage/PercentageToddlerActivity.kt
@@ -6,9 +6,9 @@ import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.components.Legend
-import com.github.mikephil.charting.data.PieData
-import com.github.mikephil.charting.data.PieDataSet
-import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.formatter.IAxisValueFormatter
+import com.github.mikephil.charting.formatter.LargeValueFormatter
 import com.github.mikephil.charting.formatter.PercentFormatter
 import com.github.mikephil.charting.utils.ColorTemplate
 import com.github.mikephil.charting.utils.MPPointF
@@ -16,6 +16,7 @@ import com.singpaulee.android_health_classification_knn.R
 import com.singpaulee.android_health_classification_knn.helper.AppContants
 import com.singpaulee.android_health_classification_knn.model.base.ToddlerModel
 import kotlinx.android.synthetic.main.activity_percentage_toddler.*
+
 
 class PercentageToddlerActivity : AppCompatActivity() {
 
@@ -29,6 +30,87 @@ class PercentageToddlerActivity : AppCompatActivity() {
 
         listClassification = intent.getParcelableArrayListExtra("listClassification")
 
+        setPieChart(listClassification)
+
+        setBarChart(listClassification)
+    }
+
+    private fun setBarChart(listClassification: ArrayList<ToddlerModel>?) {
+        val listClassificationGroup: Map<String?, List<ToddlerModel>> =
+            listClassification!!.groupBy { it.status }
+        val statusBuruk =
+            listClassificationGroup[AppContants.STATUS_MODE.STATUS_BURUK.status]?.size ?: 0
+        val statusBaik =
+            listClassificationGroup[AppContants.STATUS_MODE.STATUS_BAIK.status]?.size ?: 0
+        val statusKurang =
+            listClassificationGroup[AppContants.STATUS_MODE.STATUS_KURANG.status]?.size ?: 0
+        val statusLebih =
+            listClassificationGroup[AppContants.STATUS_MODE.STATUS_LEBIH.status]?.size ?: 0
+        val statusObesitas =
+            listClassificationGroup[AppContants.STATUS_MODE.STATUS_OBESITAS.status]?.size ?: 0
+
+        val colors: ArrayList<Int> = ArrayList()
+        for (c in ColorTemplate.VORDIPLOM_COLORS)
+            colors.add(c)
+
+        //Set Data Entries
+        val entriesBuruk: ArrayList<BarEntry> =
+            mutableListOf(BarEntry(0.toFloat(), statusBuruk.toFloat())) as ArrayList<BarEntry>
+        val barDataSetBuruk = BarDataSet(entriesBuruk, "Buruk [$statusBuruk]")
+        barDataSetBuruk.color = colors[0]
+
+        val entriesKurang: ArrayList<BarEntry> =
+            mutableListOf(BarEntry(1.toFloat(), statusKurang.toFloat())) as ArrayList<BarEntry>
+        val barDataSetKurang = BarDataSet(entriesKurang, "Kurang [$statusKurang]")
+        barDataSetKurang.color = colors[1]
+
+        val entriesBaik: ArrayList<BarEntry> =
+            mutableListOf(BarEntry(2.toFloat(), statusBaik.toFloat())) as ArrayList<BarEntry>
+        val barDataSetBaik = BarDataSet(entriesBaik, "Baik [$statusBaik]")
+        barDataSetBaik.color = colors[2]
+
+        val entriesLebih: ArrayList<BarEntry> =
+            mutableListOf(BarEntry(3.toFloat(), statusLebih.toFloat())) as ArrayList<BarEntry>
+        val barDataSetLebih = BarDataSet(entriesLebih, "Lebih [$statusLebih]")
+        barDataSetLebih.color = colors[3]
+
+        val entriesObesitas: ArrayList<BarEntry> =
+            mutableListOf(BarEntry(4.toFloat(), statusObesitas.toFloat())) as ArrayList<BarEntry>
+        val barDataSetObesitas = BarDataSet(entriesObesitas, "Obesitas [$statusObesitas]")
+        barDataSetObesitas.color = colors[4]
+
+        val barData = BarData()
+        barData.addDataSet(barDataSetBuruk)
+        barData.addDataSet(barDataSetKurang)
+        barData.addDataSet(barDataSetBaik)
+        barData.addDataSet(barDataSetLebih)
+        barData.addDataSet(barDataSetObesitas)
+
+        pta_barchart.description.isEnabled = false
+        pta_barchart.setPinchZoom(false)
+        pta_barchart.setDrawBarShadow(false)
+        pta_barchart.setDrawGridBackground(false)
+
+        val xAxis = pta_barchart.getXAxis()
+        xAxis.granularity = 1f
+        xAxis.setCenterAxisLabels(true)
+//        xAxis.setDrawGridLines(false)
+        xAxis.valueFormatter = IAxisValueFormatter { value, axis -> "" }
+
+        val leftAxis = pta_barchart.axisLeft
+        leftAxis.valueFormatter = LargeValueFormatter()
+//        leftAxis.setDrawGridLines(false)
+        leftAxis.spaceTop = 50f
+        leftAxis.axisMinimum = 0f // this replaces setStartAtZero(true)
+
+        pta_barchart.axisRight.isEnabled = false
+
+        pta_barchart.data = barData
+        pta_barchart.invalidate()
+
+    }
+
+    private fun setPieChart(listClassification: ArrayList<ToddlerModel>?) {
         val listClassificationGroup: Map<String?, List<ToddlerModel>> =
             listClassification!!.groupBy { it.status }
         val statusBuruk =
