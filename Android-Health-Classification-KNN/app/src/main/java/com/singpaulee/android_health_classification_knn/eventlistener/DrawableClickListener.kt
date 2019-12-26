db1d837285afe45399c9ed288b96eb6e8e526a9e
hanif <hanifabdullah7@gmail.com>
2019-12-27 01:16:05 +0700

    change color button to blue

--- Android-Health-Classification-KNN/app/src/main/java/com/singpaulee/android_health_classification_knn/eventlistener/DrawableClickListener.kt
+++ Android-Health-Classification-KNN/app/src/main/java/com/singpaulee/android_health_classification_knn/eventlistener/DrawableClickListener.kt
@@ -2,8 +2,6 @@ package com.singpaulee.android_health_classification_knn.eventlistener
 
 interface DrawableClickListener {
 
-    enum class DrawablePosition {
-        TOP, BOTTOM, LEFT, RIGHT
-    };
+    enum class DrawablePosition { TOP, BOTTOM, LEFT, RIGHT }
     fun onClick(target: DrawablePosition)
 }
